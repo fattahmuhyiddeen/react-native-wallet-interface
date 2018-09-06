7edf1488b43a998096b2117d501a9b03faeb51a1
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2018-09-06 14:54:48 +0800

    bug fix

--- src/modules/register/EnterPhoneScreen.js
+++ src/modules/register/EnterPhoneScreen.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   View,
   Text,
   StyleSheet,
   Image,
   TextInput,
-  TouchableOpacity,
-} from 'react-native';
-import PropTypes from 'prop-types';
-import NavBar from '../common/Header';
-import AvatarImage from './AvatarImage';
+  TouchableOpacity
+} from "react-native";
+import PropTypes from "prop-types";
+import NavBar from "../common/Header";
+import AvatarImage from "./AvatarImage";
 
-const tickImg = require('./tick.png');
-const untickImg = require('./untick.png');
-const hpImg = require('./hp.png');
+const tickImg = require("./tick.png");
+const untickImg = require("./untick.png");
+const hpImg = require("./hp.png");
 
 export default class EnterPhoneScreen extends Component {
   state = {
-    phone: __DEV__ ? '12423489' : '',
-    countryCode: '60',
-    isAgree: __DEV__,
+    phone: __DEV__ ? "12423489" : "",
+    countryCode: "60",
+    isAgree: __DEV__
   };
 
   buttonPressed = () => {
     const { store } = this.props;
     const { countryCode, phone } = this.state;
-    store.action.callApi('post', 'walletRegister', {
+    store.action.callApi("post", "walletRegister", {
       contact_number: countryCode + phone,
-      full_name: store.state.profile.full_name,
+      full_name: store.state.profile.full_name
     });
     // store.navigation.navigate("EnterTac");
   };
@@ -36,7 +36,7 @@ export default class EnterPhoneScreen extends Component {
     const { state } = store;
     const { themeColor, profile } = state;
     const { phone, countryCode, isAgree } = this.state;
-    const isFilled = phone != '' && countryCode != '' && isAgree;
+    const isFilled = phone != "" && countryCode != "" && isAgree;
     return (
       <View style={styles.container}>
         <View style={[styles.header, { backgroundColor: themeColor }]}>
@@ -52,16 +52,18 @@ export default class EnterPhoneScreen extends Component {
         {!state.isKeyboardAppear && (
           <View
             style={{
-              position: 'absolute',
+              position: "absolute",
               // left: 0,
               // right: 0,
               height: 100,
               top: 100,
-              alignSelf: 'center',
+              alignSelf: "center"
             }}
           >
             <AvatarImage />
-            <Text style={{ fontSize: 17, fontWeight: 'bold' }}>
+            <Text
+              style={{ fontSize: 17, fontWeight: "bold", textAlign: "center" }}
+            >
               {profile.full_name}
             </Text>
           </View>
@@ -70,16 +72,16 @@ export default class EnterPhoneScreen extends Component {
         <View
           style={[
             styles.scrollView,
-            { marginTop: state.isKeyboardAppear ? 30 : 140 },
+            { marginTop: state.isKeyboardAppear ? 30 : 140 }
           ]}
         >
           <View style={{ padding: 20 }}>
-            <Text style={{ color: 'grey', marginVertical: 10 }}>
+            <Text style={{ color: "grey", marginVertical: 10 }}>
               Just enter your registered local mobile number, we'll send you the
               secret code. Easy-peasy.
             </Text>
 
-            <View style={{ flexDirection: 'row', width: '100%' }}>
+            <View style={{ flexDirection: "row", width: "100%" }}>
               <View style={styles.countryCodeGroup}>
                 <Image
                   source={hpImg}
@@ -87,7 +89,7 @@ export default class EnterPhoneScreen extends Component {
                   style={{ height: 20, width: 20, marginRight: 7 }}
                 />
                 <TextInput
-                  value={countryCode}
+                  value={"+" + countryCode}
                   maxLength={3}
                   placeholder="60"
                   // editable={false}
@@ -101,7 +103,7 @@ export default class EnterPhoneScreen extends Component {
                 <TextInput
                   value={phone}
                   maxLength={9}
-                  placeholder="e.g. : 01110001000"
+                  placeholder="e.g. : 1110001000"
                   // autoFocus
                   style={styles.textInput}
                   keyboardType="phone-pad"
@@ -114,25 +116,25 @@ export default class EnterPhoneScreen extends Component {
             <TouchableOpacity
               onPress={() => this.setState({ isAgree: !isAgree })}
               style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                padding: 20,
+                flexDirection: "row",
+                alignItems: "center",
+                padding: 20
               }}
             >
               <Image
                 source={isAgree ? tickImg : untickImg}
                 style={{
-                  tintColor: isAgree ? 'green' : 'grey',
+                  tintColor: isAgree ? "green" : "grey",
                   width: 20,
                   height: 20,
-                  marginRight: 10,
+                  marginRight: 10
                 }}
                 resizeMode="contain"
               />
               <Text
                 style={{
-                  textDecorationLine: 'underline',
-                  color: isAgree ? 'green' : 'grey',
+                  textDecorationLine: "underline",
+                  color: isAgree ? "green" : "grey"
                 }}
               >
                 I agree with the terms and conditions
@@ -143,11 +145,11 @@ export default class EnterPhoneScreen extends Component {
               activeOpacity={isFilled ? 0.5 : 1}
               style={[
                 styles.button,
-                isFilled ? styles.buttonFill : styles.buttonOutline,
+                isFilled ? styles.buttonFill : styles.buttonOutline
               ]}
               onPress={isFilled ? this.buttonPressed : null}
             >
-              <Text style={{ color: isFilled ? 'white' : 'orange' }}>
+              <Text style={{ color: isFilled ? "white" : "orange" }}>
                 REGISTER
               </Text>
             </TouchableOpacity>
@@ -166,55 +168,54 @@ export default class EnterPhoneScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
-    flex: 1,
+    backgroundColor: "white",
+    flex: 1
   },
   countryCodeGroup: {
     flex: 1,
     padding: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
   },
   textInput: {
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    borderBottomWidth: 1
   },
   button: {
     borderRadius: 20,
     padding: 10,
-    alignItems: 'center',
+    alignItems: "center"
   },
   buttonOutline: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 2,
-    borderColor: 'orange',
+    borderColor: "orange"
   },
   buttonFill: {
-    backgroundColor: 'orange',
+    backgroundColor: "orange"
   },
 
   scrollView: {
-    flex: 0.8,
+    flex: 0.8
   },
   header: {
-    paddingBottom: 60,
+    paddingBottom: 60
     // borderBottomWidth: 4
     // borderBottomColor: Colors.primary
   },
   subheader: {
-    position: 'absolute',
+    position: "absolute",
     top: 65,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%"
   },
   avatar: {
     height: 70,
     width: 70,
     borderRadius: 35,
     borderWidth: 2,
-    borderColor: 'white',
-  },
+    borderColor: "white"
+  }
 });
